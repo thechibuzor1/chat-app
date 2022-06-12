103481a4158d7a5a881fe9dff030dff950ca0eae
Chibuzor <chibuzorm26@example.com>
2022-06-12 21:51:35 +0100

    backend Auth

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth.js");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req,res) => {
+    res.send("suwamelaradio!");
+})
+
+app.use('auth', authRoutes);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
