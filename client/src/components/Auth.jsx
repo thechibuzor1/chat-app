f036b2aea04f6157021987519f31b868049c9e36
Chibuzor <chibuzorm26@example.com>
2022-06-15 22:48:23 +0100

    publish to heroku and netlify

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -28,7 +28,7 @@ const Auth = () => {
 
     const {  username, password, phoneNumber, avatarURL } = form;
 
-    const URL = "http://localhost:5000/auth";
+    const URL = "https://chat-app902.herokuapp.com/auth";
 
     const {
       data: { token, userId, hashedPassword, fullName }
